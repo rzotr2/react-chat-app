5dd79727ea6a98a3edadf903bf6bb831bc6b9618
rzotr2 <pavel.lukin126@gmail.com>
2024-12-11 13:02:00 +0100

    UI is ready - Divided all page into single components - Added styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
-import {ReactElement} from "react";
-import {Button, HStack} from "@chakra-ui/react";
-import {ColorModeButton} from "./components/ui/color-mode.tsx";
+import { ReactElement } from "react";
+
+import { Box, Stack } from "@chakra-ui/react";
+import { LeftSide, RightSide } from "@components";
 
 const App = (): ReactElement => {
     return (
-        <HStack>
-            <Button bg="blueviolet">Click me</Button>
-            <Button>Click me</Button>
-            <ColorModeButton>Change color theme</ColorModeButton>
-        </HStack>
+        <Stack direction={{ md: "row" }} gap="0">
+            <Box w="30%" h='calc(100vh)'>
+                <LeftSide />
+            </Box>
+            <Box w="70%" h='calc(100vh)'>
+                <RightSide />
+            </Box>
+        </Stack>
     )
-}
+};
 
 export default App;
---
+++ src/components/left-side/chatList/chatList.tsx
@@ -0,0 +1,31 @@
+import {ReactElement} from "react";
+
+import {ChatListItem} from "./chatListItem/chatListItem.tsx";
+import {Box} from "@chakra-ui/react";
+
+export const ChatList = (): ReactElement => {
+    return (
+        <Box>
+            <ChatListItem></ChatListItem>
+            <ChatListItem></ChatListItem>
+            <ChatListItem></ChatListItem>
+            <ChatListItem></ChatListItem>
+            <ChatListItem></ChatListItem>
+            <ChatListItem></ChatListItem>
+            <ChatListItem></ChatListItem>
+            <ChatListItem></ChatListItem>
+            <ChatListItem></ChatListItem>
+            <ChatListItem></ChatListItem>
+            <ChatListItem></ChatListItem>
+            <ChatListItem></ChatListItem>
+            <ChatListItem></ChatListItem>
+            <ChatListItem></ChatListItem>
+            <ChatListItem></ChatListItem>
+            <ChatListItem></ChatListItem>
+            <ChatListItem></ChatListItem>
+            <ChatListItem></ChatListItem>
+            <ChatListItem></ChatListItem>
+            <ChatListItem></ChatListItem>
+        </Box>
+    );
+};
