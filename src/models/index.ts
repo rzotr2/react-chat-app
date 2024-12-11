e06474bf8ea51d584ead575160dc5988e1bce814
rzotr2 <pavel.lukin126@gmail.com>
2024-12-11 13:02:00 +0100

    - UI is ready - Divided all page into single components - Added styles

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,3 +1,19 @@
-export type Lol = {
-    kek: string,
+export type User = {
+    userName: string,
+    userId: string,
+    userStatus?: string,
+    userPhoneNumber: number,
+    userEmail: string,
+    userPassword: string,
+    userCountry: string
+}
+
+export type Message = {
+    messageText: string,
+    messageAuthor: string,
+    messageRecipient: string,
+    messageTimeStamp: string,
+    messageId: string,
+    messageDelivered: boolean,
+    messageReceived: boolean
 }
